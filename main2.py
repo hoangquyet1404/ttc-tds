164f442ebe32843d85be3c9807391c9c98474358
hoangquyet1404 <hoangquyetgl2005@gmail.com>
2025-07-26 10:00:52 +0700

    add

--- main2.py
+++ main2.py
@@ -15,7 +15,7 @@ API_TOKEN = "703371951353e080dde13a50207e2ff7c3fc31fe88f765c17fa11d9fd1046528"
 API_YEUMONEY = "https://yeumoney.com/QL_api.php"
 API_KEY_SERVER = "http://103.149.252.133:2000/api"
 API_KEY_PAGE = "http://sublikevip.site/key.html"
-API_NOTIFICATIONS = "https://cnc.subhatde.id.vn/apis/thongbao"
+API_NOTIFICATIONS = "http://103.149.252.133:2000/apis/thongbao"
 DEFAULT_KEY = "joonwuydeptrai"
 
 HEADERS = {
